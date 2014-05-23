cc467c9898e091e95b36b3174a46c868c3d79eda
Zach Winter <contact@zachwinter.com>
2014-05-23 13:31:01 -0400

    Autoswap can now be disabled on user interaction.

--- fader-v0.2.js
+++ fader-v0.2.js
@@ -1,7 +1,7 @@
-/*
+ /*
  * 
- *	fader.js v0.1 (c) 2014
- *	Expandable jQuery Content Fader
+ *	fader.js v0.2 (c) 2014
+ *	Expandable Fading Content Slideshow
  * 
  *	@author:  Zach Winter (contact@zachwinter.com)
  *	@license: MIT (http://www.opensource.org/licenses/mit-license.php)
@@ -14,11 +14,11 @@
 		
 
 
-		//
-		//
-		// Variables
-		//
-		//
+		/*
+		*
+		*		Variables
+		*
+		*/
 
 
 
@@ -27,26 +27,28 @@
 				
 		// Adjustable options.
 		var settings = $.extend({
-			fadeSpeed: 1500,
-			swapDelay: 7500,
-			autoSwap: true,
-			keyboard: true,
-			baseIndex: 0,
-			bullets: false,
-			fixedAspect: false,
-			prevButton: false,
-			nextButton: false,
-			breakout: false,
-			disableOnResize: true,
-			changeStart: function(data){},
-			changeEnd: function(data){}
+			fadeSpeed       : 1500,
+			swapDelay       : 7500,
+			autoSwap        : true,
+			keyboard        : true,
+			baseIndex       : 0,
+			bullets         : false,
+			fixedAspect     : false,
+			prevButton      : false,
+			nextButton      : false,
+			breakout        : false,
+			disableOnResize : true,
+			disableOnNav    : false,
+			changeStart     : function(data){},
+			changeEnd       : function(data){}
 		}, options);
-		
+
 		// Global.
-		var active   = 1,
-		    slides   = container.find('> *'),
-		    first    = container.find('> *:first-child'),
-		    changing = false,
+		var active       = 1,
+		    slides       = container.find('> *'),
+		    first        = container.find('> *:first-child'),
+		    changing     = false,
+		    autoDisabled = false,
 		    timer;
 		
 		// Check CSS Transition support; use Modernizr if available.
@@ -66,11 +68,11 @@
 
 
 
-		//
-		//
-		// Functions
-		//
-		//
+		/*
+		*
+		*		Functions
+		*
+		*/
 
 
 
@@ -82,75 +84,80 @@
 					settings.bullets.find('a').removeClass('active');
 					clicked.addClass('active');
 					swapSlide(parseInt(clicked.attr('data-target')));
+					if (settings.disableOnNav) {
+						settings.autoSwap = false;
+						autoDisabled      = true;
+					}
 				}
 				e.preventDefault();
 			});
-		}
+		};
 		
 		// Keyboard bind (left/right).
 		var keyboardBind = function(){
-			var doc = $(document);
-			doc.on('keydown', function(e){
+			$(document).on('keydown', function(e){
 				if ( e.keyCode == 37 && changing == false ) {
 					updateActive(false);
+					if (settings.disableOnNav) {
+						settings.autoSwap = false;
+						autoDisabled      = true;
+					}
 				}
 				if ( e.keyCode == 39 && changing == false ) {
 					updateActive(true);
+					if (settings.disableOnNav) {
+						settings.autoSwap = false;
+						autoDisabled      = true;
+					}
 				}
 			});
-		}
+		};
 
 		// Bind previous+next buttons.
 		var prevNextBind = function(){
 			if (settings.prevButton != false) {
 				settings.prevButton.on('click', function(e){
-					if (changing != true) {
-						updateActive(false);
-					}	
+					if (settings.disableOnNav) {
+						settings.autoSwap = false;
+						autoDisabled      = true;
+					}
+					if (changing != true) updateActive(false);
 					e.preventDefault();
 				});
 			}
 			if (settings.nextButton != false) {
 				settings.nextButton.on('click', function(e){
-					if (changing != true) {
-						updateActive(true);
-					}	
+					if (settings.disableOnNav) {
+						settings.autoSwap = false;
+						autoDisabled      = true;
+					}
+					if (changing != true) updateActive(true);
 					e.preventDefault();
 				});
 			}
-		}
+		};
 		
 		// Update active slide ("active" variable) and active bullet.
 		var updateActive = function(next){
-			if (settings.bullets != false) {
-				settings.bullets.find('a').removeClass('active');
-			}
+			if (settings.bullets != false) settings.bullets.find('a').removeClass('active');
 			if (next == true) {
 				if (active == slides.length) {
-					if (settings.bullets != false) {
-						settings.bullets.find('[data-target="1"]').addClass('active');
-					}
+					if (settings.bullets != false) settings.bullets.find('[data-target="1"]').addClass('active');
 					swapSlide(1);
 				} else {
-					if (settings.bullets != false) {
-						settings.bullets.find('[data-target="'+(active+1)+'"]').addClass('active');
-					}
+					if (settings.bullets != false) settings.bullets.find('[data-target="'+(active+1)+'"]').addClass('active');
 					swapSlide(active+1);
 				}
 			} else {
 				if (active == 1) {
-					if (settings.bullets != false) {
-						settings.bullets.find('[data-target="'+slides.length+'"]').addClass('active');
-					}
+					if (settings.bullets != false) settings.bullets.find('[data-target="'+slides.length+'"]').addClass('active');
 					swapSlide(slides.length);
 				} else {
-					if (settings.bullets != false) {
-						settings.bullets.find('[data-target="'+(active-1)+'"]').addClass('active');
-					}
+					if (settings.bullets != false) settings.bullets.find('[data-target="'+(active-1)+'"]').addClass('active');
 					swapSlide(active-1);
 				}
 			}
-		}
+		};
 			
 		// Slide swapping.
 		var swapSlide = function(slide){
@@ -174,9 +181,7 @@
 			});
 
 			// Temporarily set height of container to preserve layout.
-			if (settings.fixedAspect == false) {
-				container.css('height', container.height());
-			}
+			if (settings.fixedAspect == false) container.css('height', container.height());
 
 			// Position current slide so upcoming slide can appear above it.
 			currentSlide.css({
@@ -202,7 +207,7 @@
 					mozTransition: "opacity "+(settings.fadeSpeed/1000)+"s ease",
 					transition: "opacity "+(settings.fadeSpeed/1000)+"s ease"
 				});
-			}
+			};
 
 			// Swap slides with jQuery fades; fallback for browsers without CSS Transition support.
 			self.jQueryAnimation = function(){
@@ -212,14 +217,12 @@
 					self.finishUp();
 					currentSlide.hide();
 				});
-			}
+			};
 
 			// Shared function for both rendering methods. 
 			self.finishUp = function(){
 				upcomingSlide.css('position', 'relative');
-				if (settings.fixedAspect == false) {
-					container.css('height', 'auto');
-				}
+				if (settings.fixedAspect == false) container.css('height', 'auto');
 				active   = slide;
 				changing = false;
 				container.trigger('slidechangeend');
@@ -230,17 +233,11 @@
 					totalSlides: slides.length,
 					settings: settings
 				});
-				if (settings.autoSwap == true) {
-					auto();
-				}		
-			}
+				if (settings.autoSwap == true && autoDisabled == false) auto();
+			};
 
 			// Call rendering method based on CSS Transition support.
-			if (transitionSupport == true) {
-				self.cssAnimation();
-			} else {
-				self.jQueryAnimation();
-			}
+			transitionSupport == true ? self.cssAnimation() : self.jQueryAnimation();
 			
 		}
 		
@@ -248,11 +245,9 @@
 		var auto = function(){
 			clearTimeout(timer);
 			timer = setTimeout(function(){
-				if (changing == false) {
-					updateActive(true);
-				}
+				if (changing == false) updateActive(true);
 			}, settings.swapDelay);
-		}
+		};
 
 		// Preventing slides from changing during window resize.
 		var resizeDisable = function(){
@@ -261,28 +256,24 @@
 			    timeout  = false;
 			self.trigger = function(){
 				changing = false;
-				if (settings.autoSwap == true) {
-					auto();
-				}
+				if (settings.autoSwap == true) auto();
 			}
 			viewport.on('resize', function(){
-				if (timeout != false) {
-					clearTimeout(timeout);
-				}
+				if (timeout != false) clearTimeout(timeout);
 				changing = true;
 				timeout  = setTimeout(self.trigger, 300);
 			});
-		}
+		};
 
 		
 
 
 
-		//
-		//
-		// Initializations
-		//
-		//
+		/*
+		*
+		*		Initializations
+		*
+		*/
 
 
 
@@ -292,10 +283,7 @@
 
 			// Container initialization.
 			var containerPosition = container.css('position');
-			if (containerPosition != "absolute" || containerPosition != "relative" || containerPosition != "fixed") {
-				container.css('position', 'relative');
-			}
-			container.css('overflow', 'hidden');
+			if (containerPosition != "absolute" || containerPosition != "relative" || containerPosition != "fixed") container.css('position', 'relative');
 			if (settings.fixedAspect != false) {
 				container.css({
 					height: 0,
@@ -354,27 +342,19 @@
 			}
 			
 			// Bind keyboard.
-			if (settings.keyboard == true) {
-				keyboardBind();	
-			}
+			if (settings.keyboard == true) keyboardBind();	
 	
 			// Bind prev/next buttons.
-			if (settings.prevButton != false || settings.nextButton != false) {
-				prevNextBind();
-			}
+			if (settings.prevButton != false || settings.nextButton != false) prevNextBind();
 	
 			// Start automatic slide swapping.
-			if (settings.autoSwap == true) {
-				auto();
-			}
+			if (settings.autoSwap == true) auto();
 	
 			// Enable the prevention of slides changing during window resize.
-			if (settings.disableOnResize == true) {
-				resizeDisable();
-			}
+			if (settings.disableOnResize == true) resizeDisable();
 		
 		}
 			
 	}
 	
-})(jQuery);
+}(jQuery));
